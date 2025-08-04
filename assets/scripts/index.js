c74346a6b58e8527f54cb8a5733bd4a36abc1278
Grimshady1990 <frankiedthomas@gmail.com>
2025-08-04 22:23:55 +0100

    Give us a working board that can place markers and disallow double placements

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,11 +1,12 @@
 function Cell() {
     let value = " ";
     const getValue = () => value;
-    const addToken = (player) => {
+    const addMarker = (player) => {
         value = player;
     };
     return {
-        getValue
+        getValue,
+        addMarker
     }
 }
 
@@ -28,10 +29,47 @@ function Gameboard() {
         
         console.log(boardWithValues);
     }
+
+    const placeMarker = (row, column, player) => {
+        const availableCells = board
+        .map((line) => line[row].getValue());
+
+        if (availableCells[row][column] === "X") {
+        
+            console.log("Place marker in empty cell!");
+        }
+        else {
+            
+            board[row][column].addMarker(player);
+            printBoard();
+        }
+        
+    }
+    
     return {
-        printBoard
+        printBoard,
+        placeMarker
     }
 }
 
 const game = Gameboard();
-game.printBoard();
+game.placeMarker(0, 0, "X");
+
+game.placeMarker(0, 0, "O");
+
+game.placeMarker(0, 1, "O");
+
+game.placeMarker(0, 2, "X");
+
+game.placeMarker(1, 0, "O");
+
+game.placeMarker(1, 1, "X");
+
+game.placeMarker(1, 2, "O");
+
+game.placeMarker(2, 0, "X");
+
+game.placeMarker(2, 1, "O");
+
+game.placeMarker(2, 2, "X");
+
