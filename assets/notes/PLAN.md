6e543ae5d3251b4fcbefadd09db1132c2f09c4a0
Grimshady1990 <frankiedthomas@gmail.com>
2025-08-04 08:39:30 +0100

    Add print board function

---
+++ assets/notes/PLAN.md
@@ -0,0 +1,18 @@
+# What Needs to Happen
+
+## Create Game Board
+
+Creating the board is the first step this will be done using Array stored inside a object I will use a loop to create the board with 3 rows and three columns.
+
+## Print Game Board
+
+## Create Players
+
+## Players Can Modify Board
+
+## Turn based Mechanics
+
+## Game End
+
+## Keeping Score
+
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,37 @@
-console.log("test");
+function Cell() {
+    let value = " ";
+    const getValue = () => value;
+    const addToken = (player) => {
+        value = player;
+    };
+    return {
+        getValue
+    }
+}
+
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+    const getBoard = () => board;
+
+    const printBoard = () => {
+        const boardWithValues = board.map((row) => row.map((cell) => cell.getValue()));
+        
+        console.log(boardWithValues);
+    }
+    return {
+        printBoard
+    }
+}
+
+const game = Gameboard();
+game.printBoard();
